12b542a12b185338aadbf9c92a916bfa2415cc63
aksss15 <akerke.otarbayeva@gmail.com>
2021-03-28 02:06:02 +0600

    refactor: remove console

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    console.log(arr);
     const flatArr =  arr.reduce((acc, elem) => {
       if (Array.isArray(elem)) {
         elem.length != 0 ? acc.push(...elem) : acc.push(1);
