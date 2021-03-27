a2e105da195497153730c06086625ea1ff50aeed
aksss15 <akerke.otarbayeva@gmail.com>
2021-03-27 16:37:31 +0600

    fix: fix dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return Boolean(members) && Array.from(members).length && members.reduce((acc, elem) => {
+    if (typeof(elem) === "string") {
+      acc.push(elem.replace(/\s/g, "")[0].toUpperCase());
+    }
+    return acc.sort();
+  }, []).join("");
 };
