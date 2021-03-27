758659cf672f0ab91348fbe5b0bf7f96189ea0d0
aksss15 <akerke.otarbayeva@gmail.com>
2021-03-28 02:07:02 +0600

    fix: fix transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let resArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') {
+      i++;
+    } else if (arr[i] == '--discard-prev') {
+      if (arr[i - 2] != '--discard-next') {
+        resArray.pop();
+      }
+    } else if (arr[i] == '--double-next') {
+      resArray.push(arr[i + 1]);
+    } else if (arr[i] == '--double-prev') {
+      if (arr[i - 2] != '--discard-next') {
+        resArray.push(arr[i - 1]);
+      }
+    } else {
+      resArray.push(arr[i]);
+    }
+  }
+
+  return resArray.filter(e => e !== undefined);
 };
