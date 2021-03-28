6ccb18a61d40edeaaa5f34d2a0af88a068078f16
aksss15 <akerke.otarbayeva@gmail.com>
2021-03-28 20:33:20 +0600

    fix: fix vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("THROWN");
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let enc = "";
+    let i = 0;
+    for (let char of message) {
+      const m = char.charCodeAt(0);
+      
+      if (m >= 65 && m <= 90) {
+        const k = key.charCodeAt(i % key.length);
+        const c = (m + k) % 26 + 65;
+        char = String.fromCharCode(c);
+        i++;
+      }
+      
+      enc += char;
+    }
+    return this.isDirect ? enc : enc.split("").reverse().join("");
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error("THROWN");
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let dec = "";
+    let i = 0;
+    for (let char of encryptedMessage) {
+      const c = char.charCodeAt(0);
+      
+      if (c >= 65 && c <= 90) {
+        const k = key.charCodeAt(i % key.length);
+        const m = (c - k + 26) % 26 + 65;
+        char = String.fromCharCode(m);
+        i++;
+      }
+      
+      dec += char;
+    }
+    return this.isDirect ? dec : dec.split("").reverse().join("");
   }
 }
-
 module.exports = VigenereCipheringMachine;
